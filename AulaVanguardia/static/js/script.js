7a0e30f0c1763a5527114f07d9c3ca06f90550c6
AATPVVC <angelalvarez@liceovvh.cl>
2026-07-29 11:15:39 -0400

    ok

---
+++ AulaVanguardia/static/js/script.js
@@ -0,0 +1,8 @@
+console.log(`Conexion...`)
+
+function cambioIngles() {
+   alert("translating to inglish...")
+}
+function cambioEspañol() {
+    alert("Traduciendo a Español")
+}
---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+console.log(`Conexion`)
+
+function cambioPagina() {
+    alert(`Traducido al ingles \n(translated inglish)`)
+}
